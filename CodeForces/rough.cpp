25127d49639b5722aea71707b115385b760b9f0a
S-Swaroop <itsmesharan2000@gmail.com>
2022-09-28 21:17:41 +0530

    initial commit

---
+++ CodeForces/rough.cpp
@@ -0,0 +1,33 @@
+/*
+***************************
+* Author : SHARAN SWAROOP *
+***************************
+*/
+#include<bits/stdc++.h>
+#define endl '\n'
+#define int int64_t
+#define debug(x) cerr << #x << " : " << x << endl ;
+using namespace std ;
+
+void solve() {
+    cout << 1 << endl ; 
+}
+
+int32_t main() {
+   cin.tie(NULL) ;
+   std::ios::sync_with_stdio(false) ;
+   
+   #ifndef ONLINE_JUDGE
+   freopen("INPUT.txt","r",stdin) ;
+   freopen("OUTPUT.txt","w",stdout) ;
+   #endif
+   
+   int t = 1 ;
+   cin >> t ;/*comment this line if no test cases*/
+   for(int i = 1 ; i <= t ; i++)   solve() ; 
+    
+   double runtime = clock() * 1.0 /CLOCKS_PER_SEC ;
+   debug(runtime) ; 
+   
+   return 0 ;
+}
